c09dce190216f6089b35164d1708076544137b65
Sindhuram26 <86356100+Sindhuram26@users.noreply.github.com>
2023-07-17 08:33:08 -0700

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version =100.2f;
+		String name ="Firefox";
+		boolean isVisible=true;
+		int year=1998;
+		char logo='m';
+		
+		System.out.println("The Version is:" +version);
+		System.out.println("The Name of the browser is:" +name);
+		System.out.println(isVisible);
+		System.out.println(year);
+		System.out.println(logo);
+		
+	}
+
+}
