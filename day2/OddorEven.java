005ab6805ee3a4e8302546826194cc72d37d6a41
Sindhuja Vairakkannu <86356100+Sindhuram26@users.noreply.github.com>
2023-07-18 09:49:50 -0700

    Add files via upload

---
+++ day2/OddorEven.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class OddorEven {
+	
+	
+	
+	public  static void main(String[] args)
+	{
+ int x=10;
+ 
+ if(x%2==0) {
+	 System.out.println("The Number is " +x+" Even");
+	 
+ }
+ else {
+	 System.out.println("The Number is " +x+ "Odd");
+ }
+}
+}
